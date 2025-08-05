97c2cb5004b7b3b208d6e3a3f96dff3052b2d600
Diego Roses <diroq44@gmail.com>
2025-08-05 13:55:48 -0600

    Add first chapter, NEXT GUI INTRO

--- request_api_data/Connect_to_api.py
+++ request_api_data/Connect_to_api.py
@@ -2,22 +2,22 @@
 import json
 import requests
 
-base_url = "https://pokeapi.co/api/v2/"
+base_url = "https://pokeapi.co/api/v2/" #This is the URL we will search in
 
 def get_pokemon_info(name):
-    url = f"{base_url}/pokemon/{name}"
+    url = f"{base_url}/pokemon/{name}" #This is because on the url this is how we search for the information
     response = requests.get(url)
 
-    if response.status_code == 200:
-        pokemon_data = response.json()
+    if response.status_code == 200: #This IF statment will give us the 200 that means that evertyhing is correct if not we are not connected to the page
+        pokemon_data = response.json() #Here we are calling the information of the page as a Json dictionary
         return pokemon_data
     else:
         print(f"Faile to retrieve data {response.status_code}")
 
-pokemon_name = "pikachu"
-pokemon_info = get_pokemon_info(pokemon_name)
+pokemon_name = "metapod" #This is the name we will find or want to find
+pokemon_info = get_pokemon_info(pokemon_name) #We are asigning the fucntion to a value so we can printed
 
-if pokemon_info:
+if pokemon_info: #Here we type the information we would like to have if the pokemon was found
     print(f"Name: {pokemon_info["name"].capitalize()}")
     print(f"Id: {pokemon_info["id"]}")
     print(f"Height: {pokemon_info["height"]}cm")
